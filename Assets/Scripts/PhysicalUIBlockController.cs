37dba9cd93183052d541f5bb0c2f5c63761dac57
Daniel Rigberg <daniel@lonerooftop.com>
2020-07-28 10:36:23 +0200

    make physical UI blocks wiggle

--- Assets/Scripts/PhysicalUIBlockController.cs
+++ Assets/Scripts/PhysicalUIBlockController.cs
@@ -5,13 +5,32 @@ using TMPro;
 
 public class PhysicalUIBlockController : MonoBehaviour
 {
+    // text
     public delegate void Action(); 
     public Action action = DoNothing;
     private Canvas canvas;
     private TMPro.TextMeshProUGUI textMesh;
  
+    // movement
+    public float frequency = 6.0f;
+    public float maxRotation = 2.0f;
+
+    private float localTime = 0.01f;
+    private float targetXRotation = 0.0f;
+    private float targetZRotation = 0.0f;
+
     // Start is called before the first frame update
     void Start() {}
+
+    void Update() {
+        localTime += Time.deltaTime * frequency;
+        targetXRotation = Mathf.Sin(localTime) * maxRotation;
+        targetZRotation = Mathf.Cos(localTime + Mathf.PI) * maxRotation;
+    }
+
+    void FixedUpdate() {
+        transform.eulerAngles = new Vector3(targetXRotation, 0.0f, targetZRotation);
+    }
 
     static void DoNothing() {
         Debug.Log("No action assigned: doing nothing");
